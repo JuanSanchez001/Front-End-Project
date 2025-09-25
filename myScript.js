6c5b7599f7a3beb38049c0aa3aed6adc003a34fb
Juan Sanchez <juansanchez@Juans-MacBook-Air.local>
2025-09-24 22:01:52 -0700

    Fix: fixes an error with image1 in validator html

--- myScript.js
+++ myScript.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-$("#image1").click(function(){
+	$("#image1").click(function(){
     $("#ulp1").toggle();
   });
   
-  $("#image2").click(function(){
+  	$("#image2").click(function(){
     $("#ulp2").toggle();
   });
 });
@@ -11,25 +11,23 @@ $("#image1").click(function(){
 document.addEventListener("DOMContentLoaded", function() {
   var coll = document.getElementsByClassName("collapsible");
   for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "block";
-		let anim = content.querySelector('.timeline-animate');
-        if(anim) anim.classList.remove('active');
-		content.style.display = "none";
-      } else {
-        content.style.display = "block";
-		 setTimeout(function(){
-          let anim = content.querySelector('.timeline-animate');
-          if(anim) anim.classList.add('active');
+  coll[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "block";
+  let anim = content.querySelector('.timeline-animate');
+  if(anim) anim.classList.remove('active');
+	content.style.display = "none";
+  } else {
+    	content.style.display = "block";
+		setTimeout(function(){
+        let anim = content.querySelector('.timeline-animate');
+        if(anim) anim.classList.add('active');
         }, 10);
-		
-      }
-	  
-    });
-  }
+		 }
+	  });
+  	}
 });
 
 
